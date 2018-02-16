ae26708c057124809af2e6a314960547a3116741
Olivier Vaillancourt <olivier.vaillancourt@gmail.com>
2018-02-15 20:12:33 -0500

    Update qsAuthMiddleware.js

--- js/qsAuthMiddleware.js
+++ js/qsAuthMiddleware.js
@@ -1,1 +1,10 @@
-var t = '';
+'use strict';
+
+function createMiddleware() {
+  return function middleware(req, res, next) {
+    console.log('middleware1:', 'execute');
+    next();
+  }
+}
+
+module.exports = createMiddleware;
